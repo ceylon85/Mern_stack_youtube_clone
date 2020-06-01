dded38cf7a2a0a5415b53c8219b07c557b9528ec
Seungjun Lee <45006553+ceylon85@users.noreply.github.com>
2020-06-02 00:06:56 +0900

    Update dev.js

--- server/config/dev.js
+++ server/config/dev.js
@@ -1,3 +1,3 @@
 module.exports = {
-    mongoURI:'mongodb+srv://mongo12:mongo1234@movie-qzyzb.mongodb.net/test?retryWrites=true&w=majority'
+    mongoURI:'mongodb+srv://<username>:<password>@movie-qzyzb.mongodb.net/test?retryWrites=true&w=majority'
 }
